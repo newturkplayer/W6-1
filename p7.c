dfb23f9229dde5359d9f18b76be88d5cf2e5fdeb
Ahmet Özmen <60114994+ozmen54@users.noreply.github.com>
2021-11-05 11:53:17 +0300

    Update p7.c

--- p7.c
+++ p7.c
@@ -1,16 +1,29 @@
 // Agregate types: Arrays
 // Sending array as a parameter
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
+void fill_arr(int aa[], int l);
 void print_arr(int a[], int l);
 int get_total(int a[], int l);
 
 int main()
 {
-  int arr[4] = {1,23,64,4};
-  print_arr(arr, 4);
-  int t = get_total(arr, 4);
+  int arr[4]; // = {1,23,64,4};
+  fill_arr(arr, 4); // Fill the array with random numbers
+  print_arr(arr, 4); // Print
+  int t = get_total(arr, 4); // Get the total
   printf("Total = %d\n", t);
+  return 0;
+}
+
+
+void fill_arr(int aa[], int length){
+  srand(time(NULL));
+  for(int i = 0; i < length; i++){
+    aa[i] = (int) rand();
+  }
 }
 
 void print_arr(int aa[], int length){
